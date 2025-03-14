c2021d68c12ed21b4a1ef68956c481e6ce0f9b6a
WeiLocus <marsuo52@gmail.com>
2025-03-14 21:13:08 +0800

    feat: add header content and. reset css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import WeatherDashboard from './views/WeatherDashboard'
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div>
+      <WeatherDashboard />
+    </div>
+  )
 }
 
 export default App
---
+++ src/views/WeatherDashboard.jsx
@@ -0,0 +1,14 @@
+// Tailwind 預設斷點
+// sm: 640px, md: 768px, lg: 1024px, xl: 1280px, 2xl: 1536px
+
+function WeatherDashboard() {
+  return (
+    <div>
+      <header className="text-2xl m-auto py-3 bg-blue-400 text-slate-50 font-bold">
+        Weather Dashboard
+      </header>
+    </div>
+  )
+}
+
+export default WeatherDashboard
