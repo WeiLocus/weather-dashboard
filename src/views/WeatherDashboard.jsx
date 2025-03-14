226103ccb5a899509005ae636b9bdfc9b786d97d
WeiLocus <marsuo52@gmail.com>
2025-03-14 21:51:16 +0800

    feat: add SearchBar component and integrate it into WeatherDashboard

--- src/views/WeatherDashboard.jsx
+++ src/views/WeatherDashboard.jsx
@@ -1,3 +1,4 @@
+import SearchBar from '../components/SearchBar'
 // Tailwind 預設斷點
 // sm: 640px, md: 768px, lg: 1024px, xl: 1280px, 2xl: 1536px
 
@@ -7,6 +8,7 @@ function WeatherDashboard() {
       <header className="text-2xl m-auto py-3 bg-blue-400 text-slate-50 font-bold">
         Weather Dashboard
       </header>
+      <SearchBar />
     </div>
   )
 }
