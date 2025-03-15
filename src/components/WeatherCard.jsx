77e3189cd22875b03ce051e00e1b99d5b4f49fb1
WeiLocus <marsuo52@gmail.com>
2025-03-15 10:43:16 +0800

    feat: add WeatherCard component and integrate it into WeatherDashboard;

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import { FaHeart } from 'react-icons/fa'
+
+function WeatherCard() {
+  const [isFavorite, setIsFavorite] = useState(false)
+  const [isCelsius, setIsCelsius] = useState(true)
+
+  const temperatureC = 25
+  const temperatureF = (temperatureC * 9) / 5 + 32
+
+  return (
+    <div className="relative max-w-80 min-w-60 py-3 px-4 border-1 rounded-xl border-slate-400-500 shadow-lg m-auto bg-white">
+      <div className="flex justify-between">
+        {/* handle toggle favorite */}
+        <button
+          onClick={() => setIsFavorite(!isFavorite)}
+          className="absolute top-[18px] right-4 text-gray-400 hover:text-red-500 transition"
+          title="add to favorite"
+        >
+          <FaHeart
+            className={`w-4 h-4 ${
+              isFavorite ? 'text-red-500' : 'text-gray-400'
+            }`}
+          />
+        </button>
+        <p className="mb-1 text-xl font-bold md: mb-2 ">city name</p>
+      </div>
+
+      <div className="flex flex-col md:flex-row md:justify-between">
+        <div className="order-1 md:order-none text-start">
+          <p>current time hh:mm</p>
+          <p>Most Sunny</p>
+        </div>
+
+        {/* 溫度 + 切換按鈕 */}
+        <div className="flex md:flex-col md:gap-2 items-center gap-5 my-2 order-2 md:order-none text-start">
+          <p className="text-lg font-medium md:text-2xl">
+            {isCelsius ? `${temperatureC}°C` : `${temperatureF.toFixed(1)}°F`}
+          </p>
+
+          {/* Toggle 開關 */}
+          <button
+            onClick={() => setIsCelsius(!isCelsius)}
+            className="relative w-[60px] h-5 bg-gray-300 rounded-full flex items-center px-1 transition"
+          >
+            <span className="text-xs text-gray-700 w-6 text-center">
+              {isCelsius ? '°F' : '°C'}
+            </span>
+            {/* toggle移動 */}
+            <div
+              className={`w-4 h-4 bg-white rounded-full shadow-md transform transition ${
+                isCelsius ? 'translate-x-3' : 'translate-x-0'
+              }`}
+            />
+          </button>
+        </div>
+      </div>
+
+      <hr className="my-2 border-gray-300" />
+
+      <div className="flex items-center text-sm text-start">
+        <p className="basis-1/2">Wind Speed 12m/s</p>
+        <p className="basis-1/2">Humidity 50%</p>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherCard
--- src/views/WeatherDashboard.jsx
+++ src/views/WeatherDashboard.jsx
@@ -1,4 +1,5 @@
 import SearchBar from '../components/SearchBar'
+import WeatherCard from '../components/WeatherCard'
 // Tailwind 預設斷點
 // sm: 640px, md: 768px, lg: 1024px, xl: 1280px, 2xl: 1536px
 
@@ -9,6 +10,9 @@ function WeatherDashboard() {
         Weather Dashboard
       </header>
       <SearchBar />
+      <main className="md: m-4 p-3 max-w-5xl mx-auto h-96 bg-gray-200">
+        <WeatherCard />
+      </main>
     </div>
   )
 }
