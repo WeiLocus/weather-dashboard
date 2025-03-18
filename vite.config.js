1d210fa91ab582d248da16fe45bb4148fdad7522
WeiLocus <marsuo52@gmail.com>
2025-03-18 20:43:05 +0800

    feat: add esbuild configuration to remove console.log in production mode

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/weather-dashboard/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: '/weather-dashboard/',
+    esbuild: {
+      // 在 production 模式下移除 console.log
+      pure: mode === 'production' ? ['console.log'] : [],
+    },
+  }
 })
